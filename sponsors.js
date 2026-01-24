ae6d560f3691908342f7c2d46c39d1f6147ed21d
Wayne Ngan <wayne.ngan@gmail.com>
2026-01-24 00:09:37 -0500

    2026 main site

---
+++ sponsors.js
@@ -0,0 +1,70 @@
+function showOneSponsor(elem, url, imgsrc, alt, width, target="_blank") {
+    img = "<a href='" + url + "' target='" + target + "'><img src='" + imgsrc + "' width='" + width + "px' alt='" + alt + "'>";
+    elem.append(img);
+}
+    
+function genRandArray(max) {
+    array = new Array();
+    for (i = 0; i < max; i++) {
+        do {
+            num = Math.floor(Math.random() * max);
+        } while (array.includes(num));
+        array.push(num);
+    }
+    
+    return array;
+}
+
+function loadOneGoldSponsor(num, elem, scale) {
+    switch (num) {           
+        case 0:
+            showOneSponsor(elem, "https://www.layerinfinity.com/", "sponsors/layerinfinity.png", "Layer Infinity", 300*scale);
+            break;
+        case 1:
+            showOneSponsor(elem, "https://www.44emporium.com/", "sponsors/44emporium.png", "44 Emporium", 450*scale);
+            break;
+        case 2:
+            showOneSponsor(elem, "https://mfdyoyo.com/", "sponsors/monkeyfinger.png", "MonkeyfingeR", 290*scale);
+            break;
+        case 3:
+            showOneSponsor(elem, "https://www.c3yoyodesign.com/", "sponsors/c3yoyodesign.png", "C3yoyodesign", 400*scale);
+            break;
+        case 4:
+            showOneSponsor(elem, "https://www.yoyosam.com/", "sponsors/yoyosam.png", "YoYoSam", 300*scale);
+            break;
+        case 5:
+            showOneSponsor(elem, "https://truearcreturntops.ca/", "sponsors/TrueArc.png", "True Arc Return Tops", 300*scale);
+            break;
+        case 6:
+            showOneSponsor(elem, "https://www.gsquaredyoyos.com/", "sponsors/GSquared.png", "G Squared", 240*scale);
+            break;
+        case 7:
+            showOneSponsor(elem, "https://onedropyoyos.com/", "sponsors/onedrop.png", "OneDrop Yoyos", 410*scale);
+            break;
+        case 8:
+            showOneSponsor(elem, "https://spintektoys.com", "sponsors/spintek.png", "SpinTek Toys", 320*scale);
+            break;
+    }
+}
+
+function loadGoldSponsors(id, scale) {
+    elem = $("#"+id);
+    
+    numSponsors = 9;
+
+    order = genRandArray(numSponsors);
+    for (i = 0; i < numSponsors; i++) {
+        loadOneGoldSponsor(order[i], elem, scale);
+    }
+    
+    if (scale > 0.5) {
+        elem.children('a').children().css('margin', '15px 20px');
+    }
+}
+
+function loadSilverSponsors(id, scale) {
+    elem = $("#"+id);
+    
+    showOneSponsor(elem, "https://duncantoys.com/", "sponsors/duncan.png", "Duncan Toys", 230*scale);
+}
+
